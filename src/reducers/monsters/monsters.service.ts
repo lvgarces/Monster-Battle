b8e5bb8723988d9302eef3082c30d8c28ff658c4
lvgarces <110268449+lvgarces@users.noreply.github.com>
2025-01-26 15:39:00 -0500

    Update monsters.service.ts

--- src/reducers/monsters/monsters.service.ts
+++ src/reducers/monsters/monsters.service.ts
@@ -2,7 +2,7 @@ import { API_URL_PROD } from '../../constants/env';
 import { Monster } from '../../models/interfaces/monster.interface';
 
 const getAll = async (): Promise<Monster[]> =>
-  await fetch(`${API_URL_PROD}/monsters`).then(response =>
+  await fetch(`https://battle-of-monster-db.vercel.app/monsters`).then(response =>
     response.json(),
   );
 
