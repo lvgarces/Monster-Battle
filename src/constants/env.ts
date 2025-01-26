5f5be1ba25d2eeeb24273522b24c2218a428c640
lvgarces <110268449+lvgarces@users.noreply.github.com>
2025-01-26 14:36:26 -0500

    update db

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'http://localhost:3001';
-export const API_URL_PROD = 'http://battle-of-monsters.vercel.app/api';
+export const API_URL_PROD = 'http://battle-of-monsters-db.vercel.app/api';
