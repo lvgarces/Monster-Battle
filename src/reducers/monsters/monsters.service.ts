fa7a5c225b2a49b62784563fca5187f44621ed2f
lvgarces <110268449+lvgarces@users.noreply.github.com>
2025-01-26 15:36:55 -0500

    Update monsters.service.ts

--- src/reducers/monsters/monsters.service.ts
+++ src/reducers/monsters/monsters.service.ts
@@ -1,8 +1,8 @@
-import { API_URL, API_URL_PROD } from '../../constants/env';
+import { API_URL_PROD } from '../../constants/env';
 import { Monster } from '../../models/interfaces/monster.interface';
 
 const getAll = async (): Promise<Monster[]> =>
-  await fetch(`${API_URL || API_URL_PROD}/monsters`).then(response =>
+  await fetch(`${API_URL_PROD}/monsters`).then(response =>
     response.json(),
   );
 
