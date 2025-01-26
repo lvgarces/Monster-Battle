3f616af436c3916c33602bde08050d3bd0d60365
lvgarces <110268449+lvgarces@users.noreply.github.com>
2025-01-26 15:24:57 -0500

    Revert "Update env.ts"
    This reverts commit 12eda6b1b3244b413b79223770a0da608e6cac1a.

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'http://localhost:3001';
-export const API_URL_PROD = 'http://monster-of-battle-db.vercel.app/api';
+export const API_URL_PROD = 'http://battle-of-monsters-db.vercel.app/api';
